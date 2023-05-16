26c5bfd9df3b5555afd123f281642fd46efb7992
EstebanGomezR <63615382+EstebanGomezR@users.noreply.github.com>
2023-05-15 19:27:26 -0500

    Update taller.java

--- Estructurada/taller.java
+++ Estructurada/taller.java
@@ -1,1 +1,24 @@
-
+import java.util.Scanner;
+public class TallerMotos
+{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        UsuarioMoto usuarioMoto = new UsuarioMoto();
+        String identificacion, nombre, placa;
+        double precioRepuestos, precioMantenimiento;
+        double totalPago = 0;
+            System.out.println("Digite la identificacion del usuario: ");
+            identificacion = sc.next();
+            System.out.println("Digite el nombre del usuairo: ");
+            nombre = sc.next();
+            System.out.println("Digite la placa de la moto: ");
+            placa = sc.next();
+            System.out.println("Digite el precio de los repuestos: ");
+            precioRepuestos = sc.nextDouble();
+            System.out.println("Digite el precio del mantenimiento: ");
+            precioMantenimiento = sc.nextDouble();
+            totalPago = precioRepuestos  + precioMantenimiento;
+         System.out.println("El precio total del servicio es de : " + totalPago);
+    
+    }    
+}
